076df1c30e8e66dd5a7585e9a7077d41f078048f
Roksana Szajba <roksanaszajba@macbook-air-roksana.home>
2021-11-18 23:56:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('.input');
+const findBtn = document.querySelector('.find-btn');
+const city = document.querySelector('.city');
+const temp = document.querySelector('.temp');
+const desc = document.querySelector('.description');
+const icon = document.querySelector('.icon');
+
+
+const API_URL = 'api.openweathermap.org/data/2.5/weather?q=';
+const API_KEY = 'd4c49ebca2f02beaf0dcc44f5627145e';
+const API_UNIT = '&units=metric';
+
